f63311c37cfad61559849e7d9ba9ad62a82da1e6
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-04-28 14:53:53 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import Head from "next/head";
 import "../styles/globals.css";
 import Footer from "../components/Footer";
 import toast, { Toaster } from 'react-hot-toast';
@@ -12,6 +13,11 @@ const activeChain = "ethereum";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
+      <Head>
+        <title>Snapshot Tool</title>
+        <meta name="description" content="ERC721 NFT Collection Snapshot Tool" />
+        <link rel="icon" href="/your-icon-here.ico" />
+      </Head>
       <Toaster />
       <Component {...pageProps} />
       <Footer />
